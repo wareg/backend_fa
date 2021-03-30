6f9ad67a86c775333065240fd55a9db985b3784e
wareg <infoniabt@gmail.com>
2021-03-30 10:59:41 +0200

    filtering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import DataRow from './components/data_row';
 function App() {
 
   const [ serverData, setServerData ] = useState([]);
+  const [ showFilters, setShowFilters ] = useState(false);
+  const [ cityFilter, setCityFilter ] = useState("");
+  const [ countryFilter, setCountryFilter ] = useState("");
 
   useEffect(() => {
     Fetching();
@@ -24,13 +27,52 @@ function App() {
     })
   }
   
+  function handleFilterClick(){
+    setShowFilters(!showFilters);
+    // console.log("showFilters"); 
+    // console.log(showFilters); 
+    if (showFilters) {
+      setCityFilter("");  
+      setCountryFilter("");
+      // console.log("countryFilter"); 
+      // console.logls(countryFilter); 
+    }
+  }
+
+  function handleCityChange(e){
+    setCityFilter(e.target.value);
+  }
+
+  function handleCountryChange(e){
+    setCountryFilter(e.target.value);
+  }
+
   // <button onClick={handleClick}>Click Me</button> 
   return (
     <div className="App">
       <header className="App-header">
         <h2>CITY LIST</h2>
+        <button className="filtering" onClick={handleFilterClick}>
+        {showFilters ? "Remove Filters" : "Add Filter"}
+        </button>
+        {showFilters
+          ?
+            <div className="filter-inputs">
+              <input onChange={handleCityChange} type="text" placeholder="filter city"></input>
+              <br></br>
+              <input onChange={handleCountryChange} type="text" placeholder="filter country"></input>
+            </div>
+          : <></>
+        }
+        
         {serverData 
-          ? serverData.map((item, index) => < DataRow row={item} key={index}/>)
+          ? serverData.map((item, index) => 
+            < DataRow 
+              row={item} 
+              filter1={cityFilter} 
+              filter2={countryFilter} 
+              key={index}
+            />)
           : <></>
         }
       </header>
--- frontend/src/components/data_row.jsx
+++ frontend/src/components/data_row.jsx
@@ -9,22 +9,34 @@ function DataRow(props) {
 
   // console.log("dbrow");
   // console.log(props);
+  // console.log("filter2");
+  // console.log(props.filter2);
   return(
-    <div className="row">
-      <h4>
-        {props.row.city} 
-        <img src={props.row.country_icon} alt="country flag"/>   
-      </h4>
-      {showDetails
-        ? <>
-            <p>{`country: ${props.row.country}`}</p>
-            <p>{`province: ${props.row.province}`}</p>
-            <p>{`latitude: ${props.row.latitude}`}</p>
-            <p>{`longitude: ${props.row.longitude}`}</p>
-          </>
-        : <></>   }
-      <button onClick={handleClick}>Details</button>
-    </div>
+    <>
+    {(props.filter1 === "" && props.filter2 === "")
+      || (props.filter1 !== "" && props.filter2 === "" && props.row.city.includes(props.filter1))
+      || (props.filter2 !== "" && props.filter1 === "" && props.row.country.includes(props.filter2))
+      || (props.filter1 !== "" && props.filter2 !== "" 
+          && props.row.city.includes(props.filter1)
+          && props.row.country.includes(props.filter2))
+      ? <div className="row">
+          <h4>
+            {props.row.city} 
+            <img src={props.row.country_icon} alt="country flag"/>   
+          </h4>
+          {showDetails
+            ? <>
+                <p>{`country: ${props.row.country}`}</p>
+                <p>{`province: ${props.row.province}`}</p>
+                <p>{`latitude: ${props.row.latitude}`}</p>
+                <p>{`longitude: ${props.row.longitude}`}</p>
+              </>
+            : <></>   }
+          <button onClick={handleClick}>Details</button>
+        </div>
+      : <></>
+    }
+    </>
   )
 }
 
